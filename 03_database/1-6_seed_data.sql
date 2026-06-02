135a5f5b5e3aee888163740d8991803174c392de
karlispriednieks <priednieks.karlis@gmail.com>
2026-06-02 21:21:31 +0300

    add seed data file that contains users, tasks and audit logs

--- 03_database/1-6_seed_data.sql
+++ 03_database/1-6_seed_data.sql
@@ -1,3 +1,9 @@
--- Add test data for local development here.
 INSERT INTO users (full_name, email, role)
 VALUES ('Demo Administrator', 'admin@example.local', 'administrator');
+
+INSERT INTO tasks (title, description, status, priority, assigned_to)
+VALUES('Setting up the repository', 'Creating the initial project structure and launching Docker', 'Done', 'High', 1);
+
+INSERT INTO audit_logs(user_id, action, entity_name, entity_id)
+VALUES(1, 'Create task', 'tasks', 1);
+
