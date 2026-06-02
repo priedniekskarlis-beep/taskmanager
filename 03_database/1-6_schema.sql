766de93dc7adc66cc71dafa4099d6bd0729c1488
karlispriednieks <priednieks.karlis@gmail.com>
2026-06-02 20:23:17 +0300

    add first database schema that tracks users, their passwords and tasks

--- 03_database/1-6_schema.sql
+++ 03_database/1-6_schema.sql
@@ -1,11 +1,26 @@
 -- Create project-specific database schema here.
 -- Student must update this file during implementation.
 
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (  
+    id SERIAL PRIMARY KEY,    
     full_name VARCHAR(120) NOT NULL,
     email VARCHAR(160) NOT NULL UNIQUE,
     role VARCHAR(50) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    password hash VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(140) NOT NULL,
+    description TEXT,
+    status VARCHAR(50) NOT NULL DEFAULT 'To do',
+    priority VARCHAR(50) NOT NULL DEFAULT 'Medium',
+    due_date DATE,
+    assigned_to INT REFERENCES users(id) ON DELETE SET NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+
 );
