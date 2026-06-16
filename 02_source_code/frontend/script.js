38cd2630d9063d343fcb443fb6876922403e0c3f
karlispriednieks <priednieks.karlis@gmail.com>
2026-06-16 19:38:49 +0300

    Create tasks from Submit form via POST

--- 02_source_code/frontend/script.js
+++ 02_source_code/frontend/script.js
@@ -1,11 +1,30 @@
 console.log("script.js loaded")
 
-fetch("http://127.0.0.1:5000/tasks")
-    .then(response => response.json())
-    .then(data => data.forEach(task => {
+function renderTask(task) {
     const card = document.createElement("article")
     card.classList.add("card")
     card.textContent = task.title
     const column = document.querySelector(`.column[data-status="${task.status}"]`)
     if (column) {column.appendChild(card)}
-    }))
+}
+
+fetch("http://127.0.0.1:5000/tasks")
+    .then(response => response.json())
+    .then(data => data.forEach(task => renderTask(task)))
+
+const form = document.querySelector(".taskentry")
+    form.addEventListener("submit", event => {
+        event.preventDefault()
+        const taskTitle = document.querySelector("#task-title") .value
+        const taskStatus = document.querySelector("#task-status") .value 
+
+        fetch("http://127.0.0.1:5000/tasks", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({title: taskTitle, status: taskStatus})
+        })
+        
+        .then(response => response.json())
+        .then(newTask => renderTask(newTask))
+          
+    })
